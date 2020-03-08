63a98aca6cf7f6b872edeeff426051547650d3be
tonyminh95 <minhbui12121995@gmail.com>
2020-03-08 23:56:23 +0900

    remove bootstrap

--- src/components/bases/BasePagination.vue
+++ src/components/bases/BasePagination.vue
@@ -1,11 +1,16 @@
 <template>
-    <div class="pagination">
-        <div class="pagination__prev">Prev</div>
-        <div class="pagination__number" v-for="n in 5" :key="n">
+    <!-- <ul class="pagination">
+        <li class="pagination__prev">Prev</li>
+        <li class="pagination__number" v-for="n in 5" :key="n">
             {{ n }}
-        </div>
-        <div class="pagination__next">Next</div>
-    </div>
+        </li>
+        <li class="pagination__next">Next</li>
+    </ul> -->
+
+    <ul class="pagination" style="list-style: none; padding: 0">
+        <li class="d-inline-block">Bui Dang Minh</li>
+        <li class="d-inline-block">Bui Hai Dang</li>
+    </ul>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap.css'
 import store from '@/store/index'
 import router from './router'
 // filter
